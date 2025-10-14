dfa927701ede9c97c51f94b80d603dfe7635e287
Albertt <albertflori2007@gmail.com>
2025-10-14 16:47:49 +0200

    Preguntas amb while i Switch amb Try/catch. Falta afegit el 'alçada en el modificar dades

--- PE03_Control_Salut/PE03_Control_Salut.java
+++ PE03_Control_Salut/PE03_Control_Salut.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PE03_Control_Salut {
@@ -9,14 +10,16 @@ public class PE03_Control_Salut {
         int edad = 0;;
         double pes = 0;
         double alçada = 0;;
-        String menu;
         String opcio;
         int bucle;
         String opcio2;
         double imc;
-        String descripcio;
         int fc_max = 220;
-        int fc_obj_max = fc_max * 0.85; //arreglar
+        int fc_obj_max = (int)(fc_max * 0.85);
+        int fc_obj_min = (int)(fc_max * 0.50);
+        double aiguarecomenada = 0;
+        int datanaixement;
+        String descripcio;
         //-------------------------------------------------------
         Scanner Scanner1 = new Scanner(System.in);
         
@@ -46,6 +49,7 @@ public class PE03_Control_Salut {
                     System.out.println("Error: El nom no pot quedar buit.");;
                 }
 
+
                 System.out.println("Introdueix el teu 1r cognom: ");
                 cognom1 = Scanner1.next();
                 if (cognom1.isEmpty()) {
@@ -59,30 +63,37 @@ public class PE03_Control_Salut {
                     System.out.println("Error: El 2n cognom no pot quedar buit.");
                 }
 
+                while (!(edad > 0 && edad <= 120 && edad > -1)) {
                 System.out.println("Quina es la teva edad: ");
                 edad = Scanner1.nextInt();
                 if (!(edad <=120)) {
                     System.out.println("Error: La edat introduida no es valida.");
                 }
+                }
                 
+                while (!(pes > 0 && pes <=400 && pes > -1)) {
                 System.out.println("Quin es el teu pes (kg): ");
                 pes = Scanner1.nextDouble();
                 if (!(pes <=400)) {
                     System.out.println("Error: El pes introduit no es valid.");
-                    
+                }
                 }
 
+                while (!(alçada > 10 && alçada < 250 && alçada > -1)) {
+                
                 System.out.println("Quina es la teva altura (cm): ");
                 alçada = Scanner1.nextDouble();
+                }
             } catch (Exception InputMismatchException) {
+                System.out.println("Error: Completa els camps amb les dades correctes");
             }
-                
-            
 
                 break;
 
             case "b":
                 System.out.println("Has seleccionat Opció 'b' Modificació Dades");
+                try {
+                    
 
                     System.out.println("Quines dades vols modificar?");
                     System.out.println("a) Nom complet");
@@ -94,40 +105,62 @@ public class PE03_Control_Salut {
 
                     switch (opcio2) {
                         case "a":
-                        System.out.println("\n");
+                        try {
+                            System.out.println("\n");
+
                             System.out.println("Has seleccionat 'Nom'");
                             System.out.println("Introdueix el nou Nom: ");
                             nom = Scanner1.next();
 
+                        } catch (InputMismatchException e) {
+                            System.out.println("Error: Completa els camps amb les dades correctes");
+                        }
                             break;
                         
                         case "b":
+                        try {
                             System.out.println("\n");
+
+                            while (!(edad > 0 && edad <= 120 && edad > -1)) {
+                                
                             System.out.println("Has seleccionat 'Edat'");
                             System.out.println("Introdueix la nova Edat: ");
                             edad = Scanner1.nextInt();
+                            }
 
+                        } catch (InputMismatchException e) {
+                            System.out.println("Error: Completa els camps amb les dades correctes");
+                        }
                             break;
                         
                         case "c":
+                        try{
                             System.out.println("\n");
+                            while (!(pes > 0 && pes <=400 && pes > -1)) {
+                                
                             System.out.println("Has seleccionat 'Pes'");
                             System.out.println("Introdueix el nou Pes: ");
                             pes = Scanner1.nextInt();
+                            }
 
+                        } catch (InputMismatchException e) {
+                            System.out.println("Error: Completa els camps amb les dades correctes");
+                        }
                             break;
                     
                         default:
                             break;
                     }
-
+                } catch (Exception e) {
+                
+                }
                 break;
 
             case "c":
                 System.out.println("Has seleccionat Opció 'c' Visualitzar dades");
                 System.out.println("\n");
 
-                System.out.println("Hola, " + nom + "" + cognom1 + "" + cognom2 + "!");
+                System.out.println("Hola, " + nom + " " + cognom1 + " " + cognom2 + "!");
                 
                 imc = pes/ (alçada * alçada);
 
@@ -143,9 +176,19 @@ public class PE03_Control_Salut {
                 else if (imc >= 30) {
                     descripcio = "obesitat";
                 }
+
+                fc_max=fc_max-edad;
+                fc_obj_min=(int)(fc_max * 0.5);
+
+                System.out.println("FC màxima estimada: " + fc_max + "bpm");
+                System.out.println("Zona objectiu entre: " + fc_obj_min + " i " + fc_obj_max );
                 
-                System.out.println("FC màxima estimada: " + );
-                
+                aiguarecomenada = (35 * pes) / 1000;
+                System.out.println("Aigua diaria recomanada:"+ aiguarecomenada + " L/dia");
+
+                datanaixement = 2025 - edad ;
+                System.out.println("El teu any de naixement es: " + datanaixement);
+
                 break;
             
             case "d":
@@ -166,3 +209,5 @@ public class PE03_Control_Salut {
 
     }
 }
+
+//NECESITO PONER TRY/CATCH EN CADA SWITCH
